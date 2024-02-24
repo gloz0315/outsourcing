08f043bbf4b62bc906b938035a53590fe4f2e04c
onenyeong <a48142826@gmail.com>
2024-02-24 23:57:17 +0900

    🎨 refactor(Restaurants): Controller부분 리턴값 -> 상태코드로 변경

--- src/main/java/com/sparta/outsourcing/controller/RestaurantsController.java
+++ src/main/java/com/sparta/outsourcing/controller/RestaurantsController.java
@@ -1,10 +1,13 @@
 package com.sparta.outsourcing.controller;
 
+
 import com.sparta.outsourcing.dto.RestaurantsRequestDto;
 import com.sparta.outsourcing.dto.RestaurantsResponseDto;
+import com.sparta.outsourcing.entity.CommonResponse;
 import com.sparta.outsourcing.service.RestaurantsService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,35 +26,55 @@ public class RestaurantsController {
   private final RestaurantsService restaurantsService;
 
   @PostMapping("/restaurants")
-  public ResponseEntity<RestaurantsResponseDto> createRestaurant(
+  public ResponseEntity<CommonResponse<RestaurantsResponseDto>> createRestaurant(
       @RequestBody RestaurantsRequestDto requestDto) {
     RestaurantsResponseDto restaurantsResponseDto = restaurantsService.createRestaurant(requestDto);
 
-    return ResponseEntity.ok(restaurantsResponseDto);
+    return ResponseEntity.ok().body(
+        CommonResponse.<RestaurantsResponseDto>builder().code(HttpStatus.OK.value())
+            .message("가게 생성이 완료되었습니다").data(restaurantsResponseDto).build());
   }
 
   @GetMapping("/restaurants/{restaurantId}")
-  public ResponseEntity<RestaurantsResponseDto> getRestaurant(@PathVariable Long restaurantId) {
+  public ResponseEntity<CommonResponse<RestaurantsResponseDto>> getRestaurant(
+      @PathVariable Long restaurantId) {
     RestaurantsResponseDto restaurantsResponseDto = restaurantsService.getRestaurant(restaurantId);
-    return ResponseEntity.ok(restaurantsResponseDto);
+
+    return ResponseEntity.ok().body(
+        CommonResponse.<RestaurantsResponseDto>builder().code(HttpStatus.OK.value())
+            .message("가게 단건 조회가 완료되었습니다").data(restaurantsResponseDto).build());
   }
 
   @GetMapping("/restaurants")
-  public ResponseEntity<List<RestaurantsResponseDto>> getRestaurantList() {
+  public ResponseEntity<CommonResponse<List<RestaurantsResponseDto>>> getRestaurantList() {
     List<RestaurantsResponseDto> restaurantsResponseDto = restaurantsService.getRestaurantList();
-    return ResponseEntity.ok(restaurantsResponseDto);
+
+    return ResponseEntity.ok().body(
+        CommonResponse.<List<RestaurantsResponseDto>>builder().code(HttpStatus.OK.value())
+            .message("가게 전체 조회가 완료되었습니다").data(restaurantsResponseDto).build());
   }
+
+
   @PutMapping("/restaurants/{restaurantId}")
-  public ResponseEntity<RestaurantsResponseDto> updateRestaurant(@PathVariable Long restaurantId,
-      @RequestBody RestaurantsRequestDto restaurantsRequestDto){
-    RestaurantsResponseDto restaurantsResponseDto = restaurantsService.updateRestaurant(restaurantId,restaurantsRequestDto);
-    return ResponseEntity.ok(restaurantsResponseDto);
+  public ResponseEntity<CommonResponse<RestaurantsResponseDto>> updateRestaurant(
+      @PathVariable Long restaurantId,
+      @RequestBody RestaurantsRequestDto restaurantsRequestDto) {
+
+    RestaurantsResponseDto restaurantsResponseDto = restaurantsService.updateRestaurant(
+        restaurantId, restaurantsRequestDto);
+
+    return ResponseEntity.ok().body(
+        CommonResponse.<RestaurantsResponseDto>builder().code(HttpStatus.OK.value())
+            .message("가게 수정이 완료되었습니다").data(restaurantsResponseDto).build());
   }
 
   @DeleteMapping("/restaurants/{restaurantId}")
-  public ResponseEntity<String> deleteRestaurant(@PathVariable Long restaurantId) {
+  public ResponseEntity<CommonResponse> deleteRestaurant(@PathVariable Long restaurantId) {
     restaurantsService.deleteRestaurant(restaurantId);
-    return ResponseEntity.ok().body(("가게가 삭제되었습니다"));
+    
+    return ResponseEntity.ok().body(
+        CommonResponse.<String>builder().code(HttpStatus.OK.value())
+            .message("가게 삭제가 완료되었습니다").build());
   }
 
 }
