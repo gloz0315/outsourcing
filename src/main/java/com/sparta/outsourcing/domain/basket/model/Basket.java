f71ccbae3709a4c481be11ece6eaba993420deea
gloz0315 <qudtn0315@naver.com>
2024-02-26 21:19:40 +0900

    ✨ feat(basket): 장바구니에 대한 dto 클래스 생성

---
+++ src/main/java/com/sparta/outsourcing/domain/basket/model/Basket.java
@@ -0,0 +1,21 @@
+package com.sparta.outsourcing.domain.basket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Basket {
+  private Long memberId;
+  private Long restaurantId;
+  private Long menuId;
+  private int count;
+
+  public boolean checkRestaurantId(Long restaurantId) {
+    return this.restaurantId.equals(restaurantId);
+  }
+}
---
+++ src/main/java/com/sparta/outsourcing/domain/basket/service/dto/BasketRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.outsourcing.domain.basket.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BasketRequestDto {
+  private Long restaurantId;
+  private Long menuId;
+  private int count;
+}
