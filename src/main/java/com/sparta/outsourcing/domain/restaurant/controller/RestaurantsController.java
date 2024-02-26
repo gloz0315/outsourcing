16d51c78568e10ce5cf2477199994808d55a13a7
onenyeong <a48142826@gmail.com>
2024-02-26 21:09:38 +0900

    ♻️ refactor(payment) @Controller -> @RestController로 변경

--- src/main/java/com/sparta/outsourcing/domain/restaurant/controller/RestaurantsController.java
+++ src/main/java/com/sparta/outsourcing/domain/restaurant/controller/RestaurantsController.java
@@ -9,7 +9,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class RestaurantsController {
@@ -71,7 +71,7 @@ public class RestaurantsController {
   @DeleteMapping("/restaurants/{restaurantId}")
   public ResponseEntity<CommonResponse> deleteRestaurant(@PathVariable Long restaurantId) {
     restaurantsService.deleteRestaurant(restaurantId);
-    
+
     return ResponseEntity.ok().body(
         CommonResponse.<String>builder().code(HttpStatus.OK.value())
             .message("가게 삭제가 완료되었습니다").build());
