d56ac981d787863d1011ed598365ce55900743a5
gloz0315 <qudtn0315@naver.com>
2024-03-13 09:46:22 +0900

    🎨 style(global): QueryDSLConfig 파일 재설정

--- src/main/java/com/sparta/outsourcing/global/config/QueryDSLConfig.java
+++ src/main/java/com/sparta/outsourcing/global/config/QueryDSLConfig.java
@@ -2,15 +2,15 @@ package com.sparta.outsourcing.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QueryDSLConfig {
 
-  private final EntityManager entityManager;
+  @PersistenceContext
+  private EntityManager entityManager;
 
   @Bean
   public JPAQueryFactory jpaQueryFactory() {
